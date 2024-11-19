62bac6eb4c326c6e2c93c657f0fca6eae8a053ce
Shankarganesh B <181828491+Shankarganesh-B@users.noreply.github.com>
2024-11-19 16:09:22 +0530

    Updated to revert previous changes Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,34 @@
-import React, { useState } from "react";
-
-function Navbar() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(prevState => !prevState);
-  };
-
-  return (
-    <div className="navbar">
-      <div className="navbar__s1">
-        <h1 className="navbar__s1__title">Udemy</h1>
-      </div>
-
-      <div className="navbar__s2">
-        <i className="fa-solid fa-magnifying-glass" style={{ color: "#000000" }}></i>
-        <input placeholder="Search for anything here. Tech, Business, Art.." />
-      </div>
-
-      <div className="navbar__s3">
-        <p>Courses</p>
-
-        <div className="mylearning">
-          <p>My Learning</p>
-          <div className="mylearning__popup">
-            <p>You did not purchase anything yet.</p>
-          </div>
+function Navbar(){
+    return(
+        <div className="navbar">
+        <div className="navbar__s1">
+            <h1 className="navbar__s1__title">Udemy</h1>
         </div>
-        <i className="fa-solid fa-cart-shopping" style={{ color: "#000000" }}></i>
-        <i className="fa-solid fa-bell" style={{ color: "#000000" }}></i>
-        <i className="fa-solid fa-user" style={{ color: "#000000" }}></i>
-      </div>
 
-      {/* Hamburger Menu Icon */}
-      <div className="navbar__s4" onClick={toggleMenu}>
-        <i className="fa-solid fa-bars" style={{ color: "#b972df" }}></i>
-      </div>
+        <div className="navbar__s2">
+            <i className="fa-solid fa-magnifying-glass" style={{color: "#000000"}}></i>
+            <input placeholder="Search for anything here. Tech, Business, Art.."/>
+        </div>
 
-      {/* Mobile Menu (Conditional Rendering) */}
-      {isMenuOpen && (
-        <div className="mobile-menu">
-          <p>Courses</p>
-          <p>My Learning</p>
-          <p>Cart</
+        <div className="navbar__s3">
+            <p>Courses</p>
+            
+            <div className="mylearning">
+                <p>My Learning</p>
+                <div className="mylearning__popup">
+                    <p>You did not purchase anything yet.</p>
+                </div>
+            </div>
+            <i className="fa-solid fa-cart-shopping" style={{color: "#000000"}}></i>
+            <i className="fa-solid fa-bell" style={{color: "#000000"}}></i>
+            <i className="fa-solid fa-user" style={{color: "#000000"}}></i>
+        </div>
+        
+        <div className="navbar__s4">
+            <i className="fa-solid fa-bars" style={{color: "#b972df"}}></i>
+        </div>
+    </div>
+    )
+}
+
+export default Navbar
